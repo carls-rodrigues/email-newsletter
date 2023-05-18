e1c139832536fbfa938055f145b39a121f36b643
Carlos Rodrigues <cerf@furg.br>
2023-05-18 04:16:00 -0300

    health_check endpoint test

---
+++ tests/health_check.rs
@@ -0,0 +1,12 @@
+use zero2prod::main;
+
+#[cfg(test)]
+mod tests {
+    use crate::health_check;
+
+    #[tokio::test]
+    async fn health_check_succeeds() {
+        let response = health_check().await;
+        assert!(response.status().is_success());
+    }
+}
