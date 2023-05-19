bddaa6c1cf0bf4f9994425f8500710d8b4e89a34
Carlos Rodrigues <cerf@furg.br>
2023-05-19 04:12:31 -0300

    feat: add connection parameter to subscribe method

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,11 +1,16 @@
 use actix_web::{web, HttpResponse};
+use sqlx::PgConnection;
+
 #[derive(serde::Deserialize)]
 pub struct FormData {
     email: String,
     name: String,
 }
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+pub async fn subscribe(
+    _form: web::Form<FormData>,
+    _connection: web::Data<PgConnection>,
+) -> HttpResponse {
     println!("email: {}, name: {}", _form.email, _form.name);
     HttpResponse::Ok().finish()
 }
