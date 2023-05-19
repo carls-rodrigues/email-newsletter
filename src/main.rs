62eb785e1a161275af876080ce0d3090c783bdf1
Carlos Rodrigues <cerf@furg.br>
2023-05-19 03:35:13 -0300

    refactor: change startup method and integrate postgres

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use std::net::TcpListener;
-use zero2prod::run;
+use sqlx::{Connection, PgConnection};
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("0.0.0.0:0")?;
-    run(address)?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection)?.await
 }
